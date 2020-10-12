eb68308443b0b58ed3e5181178bc47ff60689689
CNegru <catalinalexandrunegru@gmail.com>
2020-10-12 08:48:00 -0400

    changes

--- src/@config/index.js
+++ src/@config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   sla: 5, // minutes SLA for inactive conversations
-  timeLength: 30, // length to search back from today
-  timeType: 'days', // minute | hour | day
+  timeLength: 2, // length to search back from today
+  timeType: 'hour', // minute | hour | day
   requestsPerPool: 4, // how many requests per cycle should we fire off?
 };
--- src/service.js
+++ src/service.js
@@ -25,7 +25,7 @@ const searchBuilderQuery = async (domain, pool, range, offset = 0, limit = 100, 
     const conversationHistoryRecords = get(conversations, 'conversationHistoryRecords', []);
 
     // Get conversation # closed by autoclose
-    const closedBySystem = conversationHistoryRecords.filter((conv) => conv.info.closeReason === 'TIMEOUT').length;
+    const closedBySystem = conversationHistoryRecords.filter((conv) => get(conv, 'info.closeReason', '') === 'TIMEOUT').length;
     const resumedFromInactive = resumedFromInactiveQuery(conversationHistoryRecords);
 
     // TODO: return data and calculate later - if failure, we can rerun segments
@@ -68,6 +68,7 @@ const getCampaigns = async () => {
   // Split calls into segments as configured
   const segmentCalls = segmentRequests(config);
   console.log(`Strategy: Will run ${segmentCalls.length} segments (groups) of ${config.requestsPerPool}`);
+
   // Call {requestPerPool} per each segment, a set has to finish before moving to the next segment
   // TODO: Convert to node pool when I get the chance
   for await (const segmentFrame of segmentCalls) {
